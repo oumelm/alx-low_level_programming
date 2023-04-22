57d50e2ca546ab03e6380349b4096b90047e0ee7
oumelm <omaymaelmrhasli@gmail.com>
2023-04-22 16:51:33 -0700

    c variables,if,else,while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowrcase
- * followed by a new line, except q and e
- * Return: Always 0
+ * main - prints the alphabets in lowercase
+ *followed by a new line, except q and e
+ *Return: Always 0
  */
 int main(void)
 {
 	char la;
 
-	for ("la = 'a'; la <= 'Z'; la++)
+	for (la != 'a'; la <= 'z'; la++)
 	{
 		if (la != 'e' && la != 'q')
-		       putchar(la);
+			putchar(la);
 	}
 
-	putchar('\n');
+	putchar('\n);
 
 	return (0);
 }
