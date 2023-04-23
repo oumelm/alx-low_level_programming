268ccca5bb5b2d0434cec8350952fc6fbffc43aa
oumelm <omaymaelmrhasli@gmail.com>
2023-04-22 17:02:05 -0700

    c variables,if,else,while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowrcase
+ * followed by a new line, except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
