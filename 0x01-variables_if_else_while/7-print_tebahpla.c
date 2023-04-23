27edd67d2ed793cff3b682956b3b44988faec290
oumelm <omaymaelmrhasli@gmail.com>
2023-04-23 10:35:52 -0700

    c variable,if,else,while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in reverse
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		puthar(le);
+
+	putchar('\n');
+
+	return (0);
+}
