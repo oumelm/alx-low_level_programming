ce9203ec0a6836f5eacb23954924f7ee0bae44f3
oumelm <omaymaelmrhasli@gmail.com>
2023-04-26 08:20:21 -0700

    c functions,nested loops

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,17 +1,21 @@
 #include <main.h>
+
 /**
  * main - Entry point
- * Description: 'prints _putchar to stdout'
+ * Description: 'print _putchar and a new line'
  * Return: Always 0
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
