85bc5160ed4407b505cc12ab66879178286109f8
oumelm <omaymaelmrhasli@gmail.com>
2023-04-26 07:12:35 -0700

    functions nested loops

---
+++ 0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+}
