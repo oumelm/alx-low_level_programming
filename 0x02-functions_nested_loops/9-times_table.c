da521d8cecaf315a5fd7d1417fa0a6867c06fb15
oumelm <omaymaelmrhasli@gmail.com>
2023-04-26 17:19:33 -0700

    c functions, nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include"main.h"
+
+/**
+ * time_table - prints the 9 time tables
+ *
+ * Exemple Table
+ * 0, 0, 0, 0. ..
+ * 0, 1, 2, 3. ..
+ *
+ */
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 9; num <= 9; num++)
+	{
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar('.');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			/*
+			 * put space if produt is a single number
+			 * place the first digit if its two numbers
+			 */
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48); /*get first digit*/
+			_putchar((prod % 10) + 48); /*get the second digit*/
+	}
+	_putchar('\n');
+	}
+}
