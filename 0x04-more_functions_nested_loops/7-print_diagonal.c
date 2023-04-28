4cb3e8c65a4bbfb6ab9f1238e5498be0a4b0881f
oumelm <omaymaelmrhasli@gmail.com>
2023-04-28 12:02:21 -0700

    functions, nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,11 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		for (postn 1; postn <= n; postn++)
+		for (postn = 1; postn <= n; postn++)
 		{
 			for (space = 1; space <= postn; space++)
 				_putchar(' ');
-			_putchar(92); /* equal to '/' char*/
+			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
 		}
 	}
