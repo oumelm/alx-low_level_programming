75617dd81b0de025fc131bb2251b45a20e4dd55f
oumelm <omaymaelmrhasli@gmail.com>
2023-04-28 13:41:53 -0700

    functions, nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,7 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal
  *
- * @n: is the number of times the \ character should be printed
+ * @n: is the number of times the character \ should be printed
  */
 
 void print_diagonal(int n)
