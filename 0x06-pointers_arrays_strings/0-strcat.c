abd913eb0cde9530ab39c29f3a8311f5877b7e5c
oumelm <omaymaelmrhasli@gmail.com>
2023-05-04 11:09:15 -0700

    p

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@
  * @dest: pointer to destination input
  * @src: pointer to source input
  *
- * Return: pointer to resulting string @dest
+ * Return: void
  */
 
 char *_strcat(char *dest, char *src);
@@ -20,6 +20,7 @@ char *_strcat(char *dest, char *src);
 	{
 		i++;
 	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j]
