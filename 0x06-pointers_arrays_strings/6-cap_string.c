47782cc08c57205d43f5b4ecab7f4284f35e936e
oumelm <omaymaelmrhasli@gmail.com>
2023-05-04 17:10:43 -0700

    p

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,14 +7,14 @@
  */
 char *cap_string(char *str)
 {
-	int = 0;
+	int i = 0;
 
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 
 			i++;
-		if (str[i - 1] == ' ' ||
+		if (str[i - 1] == ' ' || 
 		str[i - 1] == '\t' ||
 		str[i - 1] == '\n' ||
 		str[i - 1] == ',' ||
@@ -26,7 +26,7 @@ char *cap_string(char *str)
 		str[i - 1] == '(' ||
 		str[i - 1] == ')' ||
 		str[i - 1] == '{' ||
-		str[i - 1] == '}' ||
+		str[i - 1] == '}' || 
 		i == 0)
 			str[i] -= 32;
 		i++;
