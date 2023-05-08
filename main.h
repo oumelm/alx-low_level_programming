51f4ba243da77266a050f7a37ee7b476736e59ea
oumelm <omaymaelmrhasli@gmail.com>
2023-05-08 07:56:14 -0700

    p

--- main.h
+++ main.h
@@ -1,17 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void tim_table(void);
-int add(int, int);
-void print_to_98(int n);
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
