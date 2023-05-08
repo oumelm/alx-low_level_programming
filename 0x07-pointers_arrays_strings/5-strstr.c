5b24b2aafb9f395b77ee4adb115bbc53c101f4be
oumelm <omaymaelmrhasli@gmail.com>
2023-05-08 12:33:34 -0700

    p

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 		char *one = haystack;
 		char *two = needle;
 
-		while (*one == *two && *two == '\0')
+		while (*one == *two && *two != '\0')
 		{
 			one++;
 			two++;
@@ -23,5 +23,5 @@ char *_strstr(char *haystack, char *needle)
 		if (*two == '\0')
 			return (haystack);
 	}
-	return (0);
+	return (NULL);
 }
