6e0feac970d968b1f97a59ce5e002fbc46972fb3
oumelm <omaymaelmrhasli@gmail.com>
2023-05-08 09:20:43 -0700

    p

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,14 +5,13 @@
  * @s: pointer to put the constant
  * @b: constant
  * @n: max byte to use
- *
  * Return: pointer s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int itr;
 
-	for (itr = 0; n > 0; i++; n--)
+	for (itr = 0; n > 0; itr++, n--)
 	{
 		s[itr] = b;
 	}
