aa62954f50b299bc194b3a2df9f05aae5b2bc17a
oumelm <omaymaelmrhasli@gmail.com>
2023-05-16 15:53:28 -0700

    M

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,11 +25,12 @@ int _strlen(char *s)
 char *argstostr(int ac, char **av)
 {
 	int i = 0, nc = 0, cmpt = 0;
+	char *s
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (; i < ac; i++; nc++)
+	for (; i < ac; i++, nc++)
 		nc += _strlen(av[i]);
 	s = malloc(sizeof(char) * nc + 1);
 	if (s == 0)
@@ -37,7 +38,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++; cmpt++)
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
 			s[cmpt] = av[i][j];
 
 		s[cmpt] = '\n';
