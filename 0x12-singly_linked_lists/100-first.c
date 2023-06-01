b6d0bf984703b4852726b583ddee204ed0a98e78
oumelm <omaymaelmrhasli@gmail.com>
2023-06-01 15:01:52 -0700

    S

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * _constructor - executes before main()
+ *
+ * Return: void
+ */
+void _contructor(void)
+{
+	printf("you're beat! and yet, you must allow, \n"
+		"I bore my house upon my back!\n");
+}
