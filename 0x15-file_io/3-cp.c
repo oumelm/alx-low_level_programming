abbd32b4ea5db531477049cce5724bacf13dee7e
oumelm <omaymaelmrhasli@gmail.com>
2023-06-12 08:15:07 -0700

    F

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 #define USAGE "usage: cp file_from file_to\n"
-#define ERR_NOREAD "Error: can't read from file %s\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
-#define ERR_NOCLOSE "Error: Cna't close fd %d\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
