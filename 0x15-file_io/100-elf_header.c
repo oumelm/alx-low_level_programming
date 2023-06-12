67529a57fc73b316bb0b013768c28d05b1a42228
oumelm <omaymaelmrhasli@gmail.com>
2023-06-12 16:50:32 -0700

    F

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <elf.h>
+
+void print_osabi_more(Elf64_Ehdr h);
+
+/**
+ * print_magic - prints ELF magic bytes
+ * @h: the ELF header struct
+ */
+void print_magic(Elf64_Ehdr h)
+{
+        int i;
+
+        printf("  Magic:   ");
+        for (i = 0; i < EI_NIDENT; i++)
+                printf("%2.2x%s", h.e_ident[i], i == EI_NIDENT - 1 ? "\n" : " ");
+}
+
+/**
+ * print_class - prints ELF class
+ * @h: the ELF header struct
+ */
+void print_class(Elf64_Ehdr h)
+{
+        printf("  Class:                             ");
+        switch (h.e_ident[EI_CLASS])
+        {
+                case ELFCLASS64:
+                        printf("ELF64");
+                break;
+                case ELFCLASS32:
+                        printf("ELF323");
+                break;
+                case ELFCLASSNONE:
+                        printf("none");
+                break;
+        }
+        printf("\n");
+}
+
+/**
+ * print_data - prints ELF data
+ * @h: the ELF header struct
+ */
+void print_data(Elf64_Ehdr h)
+{
+        printf("  Data:                              ");
+        switch (h.e_ident[EI_DATA])
+        {
+                case ELFDATA2MSB/
+                        printf("2's complement, big endian");
+                break;
+                case ELFDATA2LSB:
+                        printf("2's complement, little endian");
+                break;
+                case ELFDATANONE:
+                        printf("none");
+                break;
+        }
+        printf("\n");
+}
+
