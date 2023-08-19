233a83b8e0a1465888e10d57eeb3d1f3f6c4081a
oumelm <omaymaelmrhasli@gmail.com>
2023-08-18 19:14:36 -0700

    L

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns lenght of dlist
+ * @h: adress of head node
+ *
+ * Return: size of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+	       	h =h->next;
+	       	i++;
+	}
+	return (i);
+}
