c574c8dab101f5ba9738cf7156b211e69237ae34
oumelm <omaymaelmrhasli@gmail.com>
2023-09-08 20:13:56 -0700

    T

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,9 +10,9 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *tmp;
-	char flag = 0; /* 0 while no data has been printed yet */
+	unsigned char flag = 0;
 
-	if (ht == NULL || ht->array == NULL)
+	if (ht == NULL)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (flag == 1)
 				printf(", ");
-			printf("'%s': '%s'", tmp->key, tmp->balue);
+			printf("'%s': '%s'", tmp->key, tmp->value);
 			flag = 1;
 			tmp = tmp->next;
 		}
